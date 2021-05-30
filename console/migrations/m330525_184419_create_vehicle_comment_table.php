4975acfcbd40ec2623cb558cc97c6ac0dfdc2114
abedghr <abed.ghandour7298@gmail.com>
2021-05-30 20:30:48 +0300

    master:create vehicle comment migration

--- console/migrations/m330525_184419_create_vehicle_comment_table.php
+++ console/migrations/m330525_184419_create_vehicle_comment_table.php
@@ -18,8 +18,8 @@ class m330525_184419_create_vehicle_comment_table extends Migration
         ]);
 
         $this->addPrimaryKey('vehicle_and_comment_PK', 'vehicle_comment', ['vehicle_id', 'comment_id']);
-        $this->addForeignKey('vc_vehicle_id_FK', 'vehicle_comment', 'vehicle_id', 'vehicle', 'id');
-        $this->addForeignKey('vc_comment_id_FK', 'vehicle_comment', 'comment_id', 'comment', 'id');
+        $this->addForeignKey('vc_vehicle_id_FK', 'vehicle_comment', 'vehicle_id', 'vehicle', 'id','cascade','cascade');
+        $this->addForeignKey('vc_comment_id_FK', 'vehicle_comment', 'comment_id', 'comment', 'id','cascade','cascade');
 
     }
 
